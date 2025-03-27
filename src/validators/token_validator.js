18fed311c7d5646aadbb167c4d4ffbc93aa33e9d
Paula Torres <misscotters@gmail.com>
2025-03-27 18:19:22 +0100

    token validation logic

--- src/validators/token_validator.js
+++ src/validators/token_validator.js
@@ -1,15 +1,18 @@
 import jwt from 'jsonwebtoken'
-import config from '../config';
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const authenticateToken = (req, res, next) => {
-    config()
+    const JWT_SECRET = process.env.JWT_SECRET;
+
     const token = req.header('Authorization')?.replace('Bearer ', '');
   
     if (!token) {
       return res.status(403).json({ message: 'Access denied. No token provided.' });
     }
   
-    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    jwt.verify(token, JWT_SECRET, (err, user) => {
       if (err) {
         return res.status(403).json({ message: 'Invalid or expired token.' });
       }
@@ -18,5 +21,12 @@ const authenticateToken = (req, res, next) => {
       next(); 
     });
   };
+
+  generateSocketAccessToken = ({payload}) => {
+    const JWT_SECRET = process.env.JWT_SECRET;
+    const JWT_TIME = parseInt(process.env.JWT_TIME, 10);
+
+    return jwt.sign(payload, JWT_SECRET, JWT_TIME ? {expiresIn: JWT_TIME} : {});
+  }
 
   export {authenticateToken}
